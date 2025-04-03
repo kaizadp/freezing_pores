767675f727a935735f54cadfa2d6a4365f1459d0
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-04-02 18:02:15 -0700

    initializing {targets}

---
+++ _targets.R
@@ -0,0 +1,29 @@
+# Created by use_targets().
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
+
+# Load packages required to define the pipeline:
+library(targets)
+# library(tarchetypes) # Load other packages as needed. # nolint
+
+# Set target options:
+tar_option_set(
+  packages = c("tidyverse", "googlesheets4"), # packages that your targets need to run
+  format = "rds" # default storage format
+)
+
+
+# Run the R scripts in the R/ folder with your custom functions:
+source("2-code/respiration.R")
+
+# Replace the target list below with your own:
+list(
+  tar_target(corekey, read_sheet("https://docs.google.com/spreadsheets/d/1qpfho6Z7aHYg9zkT0dMtQy7Oth5MkVtFc7O7rB45_t4")),
+  
+  # respiration
+  tar_target(licor_map, read_sheet("https://docs.google.com/spreadsheets/d/1CkMjaIOUSHGJJloa4W4ILtFO3CVoeRCgK3Cf1GTom5o/", sheet = "simplified",
+             col_types = "c"))
+#  tar_target(licor_data, import_licor_data(FILEPATH = "1-data/respiration")),
+#  tar_target(licor_processed, process_licor_data(licor_data))
+)
